1a1836bc83a2e20cddf464d9dc13353cb416131c
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-08-30 22:01:39 +0100

    chore: update global types to support vite envs as modules
    additional:
    - omit env file from git tracked

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 declare module "preline";
+
+interface ImportMetaEnv {
+    readonly VITE_APP_ID: string | undefined;
+    // more env variables...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
