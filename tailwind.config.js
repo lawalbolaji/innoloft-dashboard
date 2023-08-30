d87af63af79dad86e4b84a365ef11ec6ef5bb8e0
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2023-08-30 21:08:47 +0100

    chore: setup tailwindcss @"^3.3.3" and "preline" component library @ "^1.9.0"

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["node_modules/preline/dist/*.js", "./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("preline/plugin")],
+};
